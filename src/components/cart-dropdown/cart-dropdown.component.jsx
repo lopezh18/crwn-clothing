8631e6f1890fb3d82c2e78ce3199d36dadab22c3
Haley <haleylopez95@gmail.com>
2020-06-17 19:17:05 -0700

    added dispatch for toggleCartHidden action creator

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,8 +6,9 @@ import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, dispatch, history }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -19,7 +20,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className='empty-message'>Your cart is empty</span>
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}> GO TO CHECKOUT </CustomButton>
+    <CustomButton onClick={() => {
+      history.push('/checkout');
+      dispatch(toggleCartHidden());
+      }}>
+        GO TO CHECKOUT
+      </CustomButton>
   </div>
 )
 
